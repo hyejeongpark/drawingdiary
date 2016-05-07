17bf16bcf1fe5fcaa809e8a3c9fcb566678ea2ed
Hyejeong Park <phj1698@gmail.com>
2016-05-07 12:21:38 +0900

    Use flake8 for maintenance

--- main/views.py
+++ main/views.py
@@ -4,4 +4,4 @@ from .models import Diary
 
 def index(request):
     diary_list = Diary.objects.all()
-    return render(request, 'main/index.html', {'diary_list': diary_list,})
+    return render(request, 'main/index.html', {'diary_list': diary_list, })
