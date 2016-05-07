786130d512ef2c248354817f1e8d45c298fa0f8c
Hyejeong Park <phj1698@gmail.com>
2016-05-07 12:07:56 +0900

    Setting media

--- drawingdiary/urls.py
+++ drawingdiary/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
 import main.views
 
@@ -6,3 +8,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', main.views.index, name="index"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
