7c06e38d41463190ee839223b403f685a494aeb2
Hyejeong Park <phj1698@gmail.com>
2016-05-08 17:11:35 +0900

    Create pagination

--- main/views.py
+++ main/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Diary
 
 
 def index(request):
     diary_list = Diary.objects.all().order_by('-date')
+    paginator = Paginator(diary_list, 5)
+    page = request.GET.get('page')
+    try:
+        diary_list = paginator.page(page)
+    except PageNotAnInteger:
+        diary_list = paginator.page(1)
+    except EmptyPage:
+        diary_list = paginator.page(paginator.num_pages)
     return render(request, 'main/index.html', {'diary_list': diary_list, })
